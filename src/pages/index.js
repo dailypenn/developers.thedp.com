e86f71a5613111de4639d7508bd4d61b1ba8bf0f
peterbaile <peterchenwflms@gmail.com>
2021-01-28 22:13:01 -0500

    add dom to home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,50 @@
 import React from 'react'
+import s from 'styled-components'
 
-import { Header } from '../components'
+import { Container } from '../components'
+import { POPPINS_BOLD, POPPINS_REGULAR } from '../styles/fonts'
+
+const Hero = s.div`
+  background: linear-gradient(#FB7264, #19BAFD);
+  text-align: center;
+  color: white;
+  ${POPPINS_REGULAR}
+  padding-top: 3rem;
+  padding-bottom: 1rem;
+`
+
+const HelloWorld = s.h1`
+  ${POPPINS_BOLD}
+  font-size: 6rem;
+`
+
+const SubHead = s.p`
+  margin-top: 5rem;
+  font-size: 90%;
+`
+
+const Checkout = s.p`
+  ${POPPINS_BOLD}
+  margin-top: 2rem;
+`
 
 const Index = () => {
   return (
-    <Header />
+    <Container>
+      <Hero>
+        <span role="img" style={{ fontSize: '4rem' }}> 👋 </span>
+        <HelloWorld> Hello world. </HelloWorld>
+        <p style={{ fontSize: '2.5rem' }}>We're the DP Tech Dept</p>
+
+        <SubHead>
+          We power everything digital by The Daily Pennsylvanian, <br /> Penn's
+          independent, student-run newspaper.
+        </SubHead>
+
+        <Checkout> Check out our featured projects </Checkout>
+        
+      </Hero>
+    </Container>
   )
 }
 
