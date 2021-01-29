4644f12c70619d1bac25b2953478aa06aa72421f
peterbaile <peterchenwflms@gmail.com>
2021-01-28 22:26:27 -0500

    favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,26 +1,37 @@
 module.exports = {
   siteMetadata: {
-    title: "DP Tech",
+    title: 'DP Tech'
   },
   plugins: [
-    "gatsby-plugin-styled-components",
+    'gatsby-plugin-styled-components',
     // {
     //   resolve: "gatsby-plugin-google-analytics",
     //   options: {
     //     trackingId: "",
     //   },
     // },
-    "gatsby-plugin-sharp",
-    "gatsby-plugin-react-helmet",
-    "gatsby-transformer-sharp",
+    'gatsby-plugin-sharp',
+    'gatsby-plugin-react-helmet',
+    'gatsby-transformer-sharp',
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: "images",
-        path: "./src/images/",
+        name: 'images',
+        path: './src/images/'
       },
-      __key: "images",
+      __key: 'images'
     },
-    `gatsby-plugin-sass`
-  ],
-};
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `static/logo-cube.png`, // This path is relative to the root of the site.
+      },
+    },
+  ]
+}
