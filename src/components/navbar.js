2697ef26f2291aa5923dfab9fa1355719fbff999
peterbaile <peterchenwflms@gmail.com>
2021-01-29 13:50:59 -0500

    team json yes

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Navbar, Nav } from 'react-bootstrap'
+import { Navbar } from 'react-bootstrap'
 import s from 'styled-components'
 
 import { StyledLink } from './typography'
--- src/pages/team.js
+++ src/pages/team.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import s from 'styled-components'
 
-import { Container } from '../components'
+import { Container, PageTitle, PageDescription } from '../components'
 
 const Team = () => (
   <Container>
-
+    <div style={{ textAlign: 'center', marginTop: '2rem' }}>
+      <PageTitle> Meet the Team </PageTitle>
+      <PageDescription> .... </PageDescription>
+    </div>
   </Container>
 )
 
