1d44abf4d2bc470480808bd25a815d999027a8da
peterbaile <peterchenwflms@gmail.com>
2021-01-28 21:10:02 -0500

    add fonts

---
+++ src/styles/fonts.js
@@ -0,0 +1,24 @@
+export const POPPINS_LIGHT = `
+  font-family: 'Poppins', sans-serif;
+  font-weight: 300;
+`
+
+export const POPPINS_REGULAR = `
+  font-family: 'Poppins', sans-serif;
+  font-weight: 400;
+`
+
+export const POPPINS_MEDIUM = `
+  font-family: 'Poppins', sans-serif;
+  font-weight: 500;
+`
+
+export const POPPINS_SEMI_BOLD = `
+  font-family: 'Poppins', sans-serif;
+  font-weight: 600;
+`
+
+export const POPPINS_BOLD = `
+  font-family: 'Poppins', sans-serif;
+  font-weight: 700;
+`
