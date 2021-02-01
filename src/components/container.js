b6f81bcb9e733f5c63003d877fc88007c8ec2dc7
peterbaile <peterchenwflms@gmail.com>
2021-02-01 00:38:51 -0500

    add scroll-x hidden to wrapper

--- src/components/container.js
+++ src/components/container.js
@@ -1,14 +1,19 @@
 import React from 'react'
+import s from 'styled-components'
 
 import { Footer } from './footer'
 import { Header } from './header'
 import { NavBar } from './navbar'
 
+const Wrapper = s.div`
+  overflow-x: hidden;
+`
+
 export const Container = ({ children, title }) => (
-  <>
+  <Wrapper>
     <Header title={title} />
     <NavBar />
     {children}
     <Footer />
-  </>
+  </Wrapper>
 )
